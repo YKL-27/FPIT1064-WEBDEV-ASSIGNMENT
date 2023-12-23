eb48987e2fb34afb8329787ffe977d601f513ad6
YKL-27 <ykliang88@gmail.com>
2023-12-23 16:57:03 +0800

    231223-1655 YK: - Auto next slide for slideshow.js - Style unification

--- slideshow.js
+++ slideshow.js
@@ -1,28 +1,37 @@
 let slideNo = 1;
+// First slide start with 0
 showSlide(slideNo);
+runSlide();
 
+// Change to the next slide
 function plusSlide(n){
-    showSlide(slideNo += n);
-}
-
-function currentSlide(n){
     showSlide(slideNo += n);
 }
 
 function showSlide(n){
     let i;
-    let slide = document.getElementsByClassName("product-slide");
-    
-    if(n > slide.length){slideNo = 1;}
-    if(n < 1){slideNo = slide.length;}
+    let slides = document.getElementsByClassName("product-slide");
+    // slides = array of elements with the class name "product-slide"
 
-    for (i = 0; i < slide.length; i++) {
-        slide[i].style.display = "none";
+    if(slideNo > slides.length){slideNo = 1;}
+    if(slideNo < 1){slideNo = slides.length;}
+    // If exceed length, set back to first (1) image
+    // If smaller than first (1), set to last image
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        slides[i].className = slides[i].className.replace(" active", "")
     }
+    // For every slide in slides, set it's display to none & not active
 
-    for(i = 0; i < slide.length; i++){
-        slide[i].className = slide[i].className.replace(" active", "")
-    }
 
-    slide[slideNo - 1].style.display = "block";
+    slides[slideNo-1].style.display = "block";
+    // Set the current slide as block display to show the image
+    // slideNo - 1 is used cuz the index is actually starts with 0
+}
+
+function runSlide(){
+    setInterval(function(){
+        plusSlide(1);
+    }, 5000)
 }
